c854dc714609d25b19333658b11f071a644b440f
Alessio Crippa <alessio.crippa@gmail.com>
2024-01-10 20:36:06 +0100

    update repository

---
+++ scripts/interactive_code.R
@@ -0,0 +1,21 @@
+# You can put your comment
+
+paste("Daisy", "is the best")
+# let's see what the paste function is doing
+?paste
+paste("Daisy", "is the best", sep = "+")
+
+
+first_string <- "Daisy"
+second_string = "is the best"
+paste(first_string, second_string)
+Paste(first_string, second_string)
+
+# header 1 --------------------------------------------
+
+# example from the help page
+1:12
+paste(1:12)
+
+# longer vector
+1:100
